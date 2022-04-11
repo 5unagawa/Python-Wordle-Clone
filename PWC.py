c8ccc036ddc3c044033f451d9342e686e5df4357
5unagawa <7506834+5unagawa@users.noreply.github.com>
2022-04-11 23:42:15 +0900

    Update PWC.py
    Game is in a playable state. Need to add a function to check that the word has been correctly guessed.

--- PWC.py
+++ PWC.py
@@ -5,58 +5,20 @@
 # Letters that are in the word but in the wrong spot are shown using lowercase letters.
 # Letters that are not in the word are shown using an underscore (_)
 # Enjoy! :]
-
-import random
-
-wordSolved = False
-
-def compare(t, g, guessProgress):
-    """
-    Compare the guess with the target word, letter-by-letter.
-    If the letter is in the correct place, use a capital letter.
-    If the letter is correct but in the wrong place, use a lowercase letter.
-    Update progress
-    """
-    for i in range(5):
-        if t[i] == g[i]:
-            guessProgress[i] = t[i].upper()
-        elif g[i] in t:
-            guessProgress[i] = g[i].lower()
-        else:    
-            guessProgress[i] = '_'
-
-#begin the game by importing the vocabulary and selecting a word
-def initialise:
-    wordList = ['apple', 'break', 'catch', 'dance', 'enter', 'fruit']
-    targetWord = list(wordList[random.randint(0,len(vocab))])
-    progress = ['_'] * 5
-    wordSolved = False
-    return targetWord
-
-#get input
-#check if any letters are in the same position
-#reprint word with matches
-while wordSolved == False:
-        guess = input("please guess a 5-letter word:\n")
-        guessList = list(guess)
-        compare(targetWord, guessList, progress)
-        print(progress)
-        
-    ////////////////////////////////////////////////////////////////////
-# A Python Wordle clone
-
 import random
 
 class Game:
     def __init__(self):
         self.dictionary = ['apple', 'break', 'catch', 'dance', 'enter', 'fruit', 'grape']
+        self.wordSolved = False
         self.gameOver = False
         self.remainingTurns = 6
         self.targetWord = None
         return None
 
-    # Compare the guess with the target word.
-    def compareGuess(t, g, guessProgress):
+    def compareGuess(self,g):     #Compare the guess with the target word.
+        t = list(self.targetWord)
+        guessProgress = ['_'] * 5
         for i in range(5):
             if t[i] == g[i]:
                 guessProgress[i] = t[i].upper()
@@ -64,31 +26,29 @@ class Game:
                 guessProgress[i] = g[i].lower()
             else:    
                 guessProgress[i] = '_'
+        print(guessProgress)
 
-    #begin the game by importing the vocabulary and selecting a word
-    def start():
-        targetWord = list(wordList[random.randint(0, 5)])
-        progress = ['_'] * 5
-        wordSolved = False
-        return targetWord
+    def start(self): #Begin the game by selecting a word.
+        self.targetWord = list(self.dictionary[random.randint(0, 5)])
+        self.wordSolved = False
+        print(self.targetWord)
 
 NewGame = Game()
 NewGame.start()
 while NewGame.gameOver == False:
-    playerGuess = input("please guess a 5-letter word:\n")
-        guessList = list(guess)
-        compare(targetWord, guessList, progress)
-        print(progress)
-        
-    
-    
-    
-    
-    
-    
-        
-    
-    
-    
-    
+    while True:
+        playerGuess = input("Please guess a 5-letter word:\n")
+    #validate user input
+    #check that the guess is 5 letters long
+        if len(playerGuess) != 5:
+            print("Sorry, please make sure the word is 5 letters only.")
+            continue
+        else:
+            break
+    #check that there are no numbers
+    #check that there are no special characters
+    #(later)check that the input is a real word
+    #convert guess to lower case
+    guessList = list(playerGuess)
+    NewGame.compareGuess(guessList)
     
